e7b6dc0f618569db033902da86fe726c9ebfa3b2
diegobrunetto <diego.brunetto@hotmail.com>
2020-06-22 20:15:55 -0300

    otra opcion para las cards en mobile

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,12 +5,16 @@ import './Card.css';
 export default function Card({ href, title, src, alt, color }) {
   return (
     <a href={href} className={`${color} card`}>
-      <div className='title-container'>
-        <h3>{title}</h3>
-        <img src={src} alt={alt} className='icon' />
-      </div>
-      <div className='description-container'>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+      <h3>{title}</h3>
+      <div className='container'>
+        <img
+          src={src}
+          alt={alt}
+          className='icon'
+        />
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        </p>
       </div>
     </a>
   );
